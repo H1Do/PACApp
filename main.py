f4165b04ec69a295be27f95d929c7e9f9103dee2
H1Do <90624145+H1Do@users.noreply.github.com>
2023-05-21 22:31:15 +0500

    Работа завершена. Добавлены несложные стили

--- main.py
+++ main.py
@@ -5,9 +5,63 @@ import pandas as pd
 app = Flask(__name__)
 
 place_holder = '''
+                <head>
+                <title>Поиск игроков FIFA-2021</title>
+                <style>
+                    table {
+                        width: 100%;
+                        margin-bottom: 20px;
+                        border: 5px solid #fff;
+                        border-top: 5px solid #fff;
+                        border-bottom: 3px solid #fff;
+                        border-collapse: collapse; 
+                        outline: 3px solid #ffd300;
+                        font-size: 15px;
+                        background: #fff!important;
+                    }
+                    table th {
+                        font-weight: bold;
+                        padding: 7px;
+                        background: #ffd300;
+                        border: none;
+                        text-align: left;
+                        font-size: 15px;
+                        border-top: 3px solid #fff;
+                        border-bottom: 3px solid #ffd300;
+                    }
+                    table td {
+                        padding: 7px;
+                        border: none;
+                        border-top: 3px solid #fff;
+                        border-bottom: 3px solid #fff;
+                        font-size: 15px;
+                    }
+                    table tbody tr:nth-child(even){
+                        background: #f8f8f8!important;
+                    }
+                    label {
+                        text-align: left;
+                        font-size: 30px;
+                        border-top: 3px solid #fff;
+                        border-bottom: 3px solid #ffd300;
+                    }
+                    input[type = "text"] {
+                        font-size: 30px;
+                        width: 300px;
+                        height: 40px;
+                    }
+                    input[type = "submit"] {
+                        height: 40px;
+                        font-size: 30px;
+                        width: 120px;
+                        text-align: center;
+                        background-color: rgb( 43, 153, 91 );
+                    }
+                </style>
+                </head>
                 <form method="post">
                     <label>Введите имя игрока или название команды:</label>
-                    <input type="text" name="text" value="{}">
+                    <input type="text" name="text" value="">
                     <input type="submit" value="Найти">
                 </form>
                 '''
@@ -25,8 +79,8 @@ def index():
 
         index_html = place_holder + result_html
         
-        return index_html.format('', '')
-    return place_holder.format('', '')
+        return index_html
+    return place_holder
 
 
 if __name__ == '__main__':
